a72a3f49ec040d89299f33200bc1035d745bff83
Manuel Gantiva <ing.manuel.gantiva@gmail.com>
2025-01-29 20:40:55 +0100

    Correct lissajous 2 desired path

--- asv_library/src/curvas_alamillo.cpp
+++ asv_library/src/curvas_alamillo.cpp
@@ -714,9 +714,9 @@ Target curva_lissajous_1(float w){
 //Trayectoria Paper 2
 Target curva_lissajous_2(float w){
     Target result;
-    result.yp = -2.0 + 10.0 * sin(-w + 1.5707963268); 
+    result.yp = -2.0 + 12.0 * sin(-w + 1.5707963268); 
     result.xp = -12.0 + 10.0 * sin(-2.0 * w); 
-    result.dyp = -10.0 * sin(w);
+    result.dyp = -12.0 * sin(w);
     result.dxp = -20.0 * cos(-2.0 * w); 
     result.f_c = 1; // no se utiliza en el MLC
     return result;
