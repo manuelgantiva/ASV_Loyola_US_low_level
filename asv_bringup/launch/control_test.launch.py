d9afd4f93889192a26a795ca8fdbfabc6d351b33
FAKHAR SHAHREZA SAJJAD <165798228+SajjadFakhar@users.noreply.github.com>
2026-04-05 18:42:02 +0200

    Modify control_test.launch.py node configuration
    Comment out observer_zono node and add ukf_multi_rate_node.

--- asv_bringup/launch/control_test.launch.py
+++ asv_bringup/launch/control_test.launch.py
@@ -370,8 +370,9 @@ def generate_launch_description():
     nodes.append(observer_core)
     # nodes.append(observer_bejarano)
     # nodes.append(observer_liu)
-    nodes.append(observer_zono)
-
+    # nodes.append(observer_zono)
+    nodes.append(ukf_multi_rate_node)
+    
     ###################################################################
     ##-----------------------Control Nodes---------------------------##
     ################################################################### 
