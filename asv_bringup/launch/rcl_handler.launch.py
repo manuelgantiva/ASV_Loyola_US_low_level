32ce4067b68fff4048007dbd4c89e15b1e3830b6
Sajjad Fakhar Shahreza <sajjadfakhar132@gmail.com>
2026-05-20 09:37:48 +0200

    Commenting everything execpt the rc_handler

--- asv_bringup/launch/rcl_handler.launch.py
+++ asv_bringup/launch/rcl_handler.launch.py
@@ -344,11 +344,11 @@ def generate_launch_description():
     ##-----------------------Observer Nodes--------------------------##
     ################################################################### 
     # nodes.append(mux_obs_node)
-    nodes.append(observer_core)
+    # nodes.append(observer_core)
     # nodes.append(observer_bejarano)
     # nodes.append(observer_liu)
-    nodes.append(observer_zono)
-    nodes.append(ukf_multi_rate_node)
+    # nodes.append(observer_zono)
+    # nodes.append(ukf_multi_rate_node)
 
     ###################################################################
     ##-----------------------Control Nodes---------------------------##
