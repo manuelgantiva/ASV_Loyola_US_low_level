c596d793117188523da45b95bdd68530561e25a0
Sajjad Fakhar Shahreza <sajjadfakhar132@gmail.com>
2026-06-23 15:36:47 +0200

    That was a mistake of the workload

--- install/asv_interfaces/local/lib/python3.10/dist-packages/asv_interfaces/srv/__init__.py
+++
@@ -1,2 +0,0 @@
-from asv_interfaces.srv._set_llc import SetLlc  # noqa: F401
-from asv_interfaces.srv._set_obs import SetObs  # noqa: F401
--- install/asv_interfaces/share/asv_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(asv_interfaces_IDL_FILES "msg/PwmValues.idl;msg/StateNeighbor.idl;msg/StateObserver.idl;msg/XbeeObserver.idl;msg/ReferenceLlc.idl;srv/SetObs.idl;srv/SetLlc.idl")
-set(asv_interfaces_INTERFACE_FILES "msg/PwmValues.msg;msg/StateNeighbor.msg;msg/StateObserver.msg;msg/XbeeObserver.msg;msg/ReferenceLlc.msg;srv/SetObs.srv;srv/SetObs_Request.msg;srv/SetObs_Response.msg;srv/SetLlc.srv;srv/SetLlc_Request.msg;srv/SetLlc_Response.msg")
